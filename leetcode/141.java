69b96333068bf0851eed4e649ea375ce05b5418e
Yuan Zhang <bigjackCN@gmail.com>
2026-09-20 10:03:08 -0400

    update leetcode

--- leetcode/141.java
+++ leetcode/141.java
@@ -1,6 +1,6 @@
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if (head == null) return false;
+        if (head == null || head.next == null) return false;
         ListNode slow = head;
         ListNode fast = head;
         while (fast.next!= null && fast.next.next != null) {
